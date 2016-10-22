484fc6275491929fdc10416891aee01fb891d42b
jordanvogel <jordan.n.vogel@gmail.com>
2016-10-22 15:12:11 -0600

    Progress on first Doctor Function 1

--- DoctorModule.py
+++ DoctorModule.py
@@ -1,12 +1,45 @@
-# Author : Daniel Zhou
+# Author : Daniel Zhou, Jordan Vogel
 # Function : Module to handle tasks related to doctors.
 
 import sqlite3
 import datetime
+from operator import itemgetter
 
 def DOC_A(conn,StaffID,StaffName):
-	print("Function A has been called.")
+	cursor = conn.cursor()
+	patient_name = raw_input ("Enter patient hcno: ")
+	print ("Charts for patient number " + patient_name + ":")
+	chart_number_list = []
+	
+	if patient_name.isdigit():
+		patient_name = int(patient_name)
+		cursor.execute("SELECT * FROM charts WHERE charts.hcno = '%d'" % patient_name)
+		chart_list = cursor.fetchall()
+		sorted(chart_list, key = itemgetter(2))
+		
+		for i in chart_list:
+			chart_number_list.append(int(i[0]))
+			
+			if i[3] == None:				
+				print("Chart " + i[0] + " is open")
+			else:
+				print("Chart " + i[0] + " is closed")
+		
+	else:
+		cursor.execute("SELECT * FROM charts, patients WHERE patients.name = '%s' AND charts.hcno = patients.hcno" % patient_name)
+		print(cursor.fetchall())
+		
+	print("====")
+	select_chart = int(raw_input ("Enter desired chart number: "))
+	
+	if select_chart not in chart_number_list:
+		print ("Invalid chart number entry")
+	else:
+		cursor.execute ("SELECT * FROM charts, symptoms, diagnoses, medications WHERE symptoms.chart_id = '%d' AND medications.chart_id = '%d' AND diagnoses.chart_id = '%d'" % select_chart)
+		print(cursor.fetchall())
+
 	return 0
+
 def DOC_B(conn,StaffID,StaffName):
 	print("Function B has been called.")
 	return 0
